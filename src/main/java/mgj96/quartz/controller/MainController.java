5ff563a9d677d514e038f304300961937974af14
mgj96 <akrhkdwp2789@gmail.com>
2023-06-12 00:18:20 +0900

    update main

--- src/main/java/mgj96/quartz/controller/MainController.java
+++ src/main/java/mgj96/quartz/controller/MainController.java
@@ -8,7 +8,6 @@ public class MainController {
 
     @RequestMapping(value = "/main")
     public String main(){
-
         return "main";
     }
 }
---
+++ src/main/java/mgj96/quartz/service/mainService.java
@@ -0,0 +1,7 @@
+package mgj96.quartz.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface mainService {
+}
