9ae80ffd894d4a462e0f98aaf8a182853680ac30
mgj96 <akrhkdwp2789@gmail.com>
2023-06-19 02:13:32 +0900

    update logBack

--- src/test/java/mgj96/quartz/QuartzApplicationTests.java
+++ src/test/java/mgj96/quartz/QuartzApplicationTests.java
@@ -1,11 +1,15 @@
 package mgj96.quartz;
 
+import lombok.extern.slf4j.Slf4j;
 import mgj96.quartz.message.service.ServiceImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class QuartzApplicationTests {
 
@@ -15,9 +19,9 @@ class QuartzApplicationTests {
 	@Test
 	void contextLoads() {
 
-		System.out.println("보낸 메세지 건수: " + service.checkMessage());
-		System.out.println("보낸 메세지 보내기 서비스: " + service.messageService());
-		System.out.println("보낸 메세지 건수: " + service.checkMessage());
+		log.debug("보낸 메세지 건수: {}", service.checkMessage());
+		log.debug("보낸 메세지 보내기 서비스: {}", service.messageService());
+		log.info("보낸 메세지 건수: {}", service.checkMessage());
 
 		Assertions.assertEquals(1, service.checkMessage());
 	}
